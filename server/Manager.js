e8f5df6c3cf6a31e76b2a88b9f13fcd44f75e0fd
pvdthien310 <71278418+pvdthien310@users.noreply.github.com>
2021-09-29 13:24:43 +0700

    code server

---
+++ server/Manager.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser')
+const KnowledgeRoute = require('./routes/knowledgeRoute')
+
+/// Process file json and env
+app.use(bodyParser.json())
+dotenv.config();
+
+/// Connect MongoDB
+mongoose.connect(process.env.MONGO_URL)
+mongoose.connection.on("connected", () => {
+    console.log("MongoDB Connected !")
+})
+mongoose.connection.on("error", () => {
+    console.log("Connect MongoDB Failed !")
+})
+
+
+
+/// Handle All API 
+app.use("/api/knowledge", KnowledgeRoute )
+
+
+
+
+
+
+
+//// Open port
+const port = process.env.PORT || 3001
+app.listen(port, ()=>{
+    console.log('Backends server is running!')
+});
---
+++ server/models/Knowledge.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const KnowledgeSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        min: 1 ,
+        max: 20,
+    },
+    body:{
+        type:String,
+        required:true,
+    },
+    avatar:{
+        type:String,
+        required:true,
+    },
+    posttime:{
+        type:String,
+    },
+    listImage:{
+        type: Array,
+        default:[]
+    },
+    react:{
+        type:Boolean,
+        default:false
+    },
+    reactNumber:{
+        type:String,
+    }
+},
+ {timestamps:true}
+);
+
+module.exports = mongoose.model("Knowledge", KnowledgeSchema);
