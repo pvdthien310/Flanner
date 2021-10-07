b77de545134295124fbe960fb7cf23b603a9406a
pvdthien310 <71278418+pvdthien310@users.noreply.github.com>
2021-10-07 15:31:53 +0700

    ok

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import * as React from 'react';
+import React from 'react';
 import LoginStack from './routes/loginStack';
 import { useFonts } from 'expo-font';
 
 export default function App() {
   const [loaded] = useFonts({
     capricaScript: require('./assets/fonts/CAPRICA_SCRIPT.ttf'),
-    nunitobold : require('./assets/fonts/Nunito-Bold.ttf'),
-    nunitoregular : require('./assets/fonts/Nunito-Regular.ttf'),
+    nunitobold: require('./assets/fonts/Nunito-Bold.ttf'),
+    nunitoregular: require('./assets/fonts/Nunito-Regular.ttf'),
   });
 
   if (!loaded) {
