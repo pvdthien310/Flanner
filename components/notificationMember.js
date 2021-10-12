c7c9334b4806d2183581e63459fb48ddc3af8725
pvdthien310 <71278418+pvdthien310@users.noreply.github.com>
2021-10-13 01:11:03 +0700

    ok

--- components/notificationMember.js
+++ components/notificationMember.js
@@ -6,40 +6,109 @@ import { images, imagespost, Poststyle, Poststyle_Status } from '../styles/posts
 import { Ionicons } from '@expo/vector-icons';
 import { Octicons } from '@expo/vector-icons';
 import react from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
 
 
 
 const NotificationMember = ({ item, navigation }) => {
-    const url_1 = 'http://192.168.0.106:3000/api/knowledge/' + item.postID;
-    const url_2 = 'http://192.168.0.106:3000/api/knowledge/' + item.postID;
-    const url_3 = 'http://192.168.0.106:3000/api/knowledge/' + item.postID;
-    const user_url = 'http://192.168.0.106:3000/api/knowledge/' + item.postID;
 
-    const fetchData = () => {
-        console.log(url)
+    const dispatch = useDispatch()
+    const user_data = useSelector(state => { return state.User })
+    const knowledge_data = useSelector(state => { return state.Knowledge })
+    const sender = user_data.data.filter(member => member.userID == item.senderID)
+    const post = knowledge_data.data.filter(member => member._id == item.postID)
 
-        fetch(url)
-            .then(res => res.json())
-            .then(result => {             
-                dispatch({ type: 'ADD_USER_KNOWLEDGE_NOTIFICATION', payload: result })
-                dispatch({ type: 'SET_LOADING_NOTIFICATION', payload: false })
-            }).catch(err => console.log('Error'));
-    }
+    console.log(sender[0])
+    console.log(post[0])
+    console.log(item.postID)
+    console.log('   ')
+
+
+
+    // const url_1 = 'http://192.168.0.106:3000/api/knowledge/' + item.postID;
+    // const url_2 = 'http://192.168.0.106:3000/api/knowledge/' + item.postID;
+    // const url_3 = 'http://192.168.0.106:3000/api/knowledge/' + item.postID;
+    // const user_url = 'http://192.168.0.106:3000/api/knowledge/' + item.postID;
+
+    // const fetchData = () => {
+    //     console.log(url)
+
+    //     fetch(url)
+    //         .then(res => res.json())
+    //         .then(result => {             
+    //             dispatch({ type: 'ADD_USER_KNOWLEDGE_NOTIFICATION', payload: result })
+    //             dispatch({ type: 'SET_LOADING_NOTIFICATION', payload: false })
+    //             const sender = user_data.data.filter(member => member.userID == item.senderID)
+    //         }).catch(err => console.log('Error'));
+    // }
     const HandelOpenPost = () => {
         if (item.type == '2')
-            navigation.navigate('Status Detail Notification',{item :{_id: item.postID}});
-        else if (item.type == '1') 
-            navigation.navigate('Knowledge Detail Notification',{item :{_id: item.postID}});
+            navigation.navigate('Status Detail Notification', { item: { _id: item.postID } });
+        else if (item.type == '1')
+            navigation.navigate('Knowledge Detail Notification', { item: { _id: item.postID } });
     }
 
     return (
-        <TouchableOpacity activeOpacity = { item.type == '3' ? 1 : 0.7}  onPress={() => HandelOpenPost()}>
-            <View style = {item.type == '1' ? styles.frame_1 : item.type == '2' ? styles.frame_2 : styles.frame_3  } >
-            <Text style ={styles.body}>{item.postID}</Text>
-            <Text style ={styles.body}>{item.message}</Text>
-            <Text style ={styles.body}>{item.senderID}</Text>
-            <Text style ={styles.body}>{item.type}</Text>
-            </View >
+        <TouchableOpacity activeOpacity={item.type == '3' ? 1 : 0.7} onPress={() => HandelOpenPost()}>
+            <View style={item.type == '1' ? styles.frame_1 : item.type == '2' ? styles.frame_2 : styles.frame_3} >
+            {/* <Text style={styles.body}> {item.message}</Text> */}
+
+                {
+                    sender[0].avatar ? 
+                  
+                    
+                <Image source={{ uri: sender[0].avatar }}
+                    resizeMode='stretch'
+                    style={{
+                        width: 50,
+                        height: 50,
+                        marginBottom: 5,
+                        borderRadius: 10
+                    }
+                    }
+                />
+                :
+                <Image source={require('../assets/icon/userPhoto.png')}
+                resizeMode='contain'
+                style={{
+                    width: 50,
+                    height: 50,
+                    marginBottom: 5,
+                    borderRadius:10
+                }
+                }
+            />
+                } 
+                <View style ={{ padding: 10, flexShrink: 1}}>
+                    <Text style={styles.body}><Text style={{ color: 'black', fontFamily: 'nunitobold' }}>{sender[0].name}</Text> {item.message}</Text>
+                </View>
+                
+                {/* {
+                    post[0].listImage ? 
+                 <Image source={{ uri: post[0].listImage[0].url }}
+                    resizeMode='stretch'
+                    style={{
+                        width: 50,
+                        height: 50,
+                        marginBottom: 5,
+                        borderRadius: 10
+                    }
+                    }
+                />
+                : */}
+                <Image source={require('../assets/icon/postPhoto.png')}
+                resizeMode='contain'
+                style={{
+                    width: 50,
+                    height: 50,
+                    marginBottom: 5,
+                    borderRadius:10
+                }
+                }
+            />
+            {/* } */}
+             </View > 
         </TouchableOpacity>
 
     )
@@ -56,6 +125,9 @@ const styles = StyleSheet.create({
         padding: 10,
         margin: 5,
         backgroundColor: 'lightslategrey',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignContent: 'center'
     },
     frame_2: {
         shadowOffset: { width: 1, height: 1 },
@@ -64,7 +136,10 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         padding: 10,
         margin: 5,
-        backgroundColor: 'dimgrey'
+        backgroundColor: 'dimgrey',
+        flexDirection: 'row'
+
+
     },
     frame_3: {
         shadowOffset: { width: 1, height: 1 },
@@ -73,11 +148,18 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         padding: 10,
         margin: 5,
-        backgroundColor: 'maroon'
+        backgroundColor: 'maroon',
+        flexDirection: 'row'
+
     },
-    body : {
+    body: {
         fontFamily: 'nunitoregular',
         fontSize: 15,
         color: 'white'
+    },
+    imagepost: {
+        height: 50,
+        width: 50,
+        resizeMode: 'stretch'
     }
 })
