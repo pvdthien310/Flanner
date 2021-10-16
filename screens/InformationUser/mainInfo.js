2f3d8f5c81fe3c272ea6733ede26bab4e2107444
pvdthien310 <71278418+pvdthien310@users.noreply.github.com>
2021-10-16 13:36:48 +0700

    ok

--- screens/InformationUser/mainInfo.js
+++ screens/InformationUser/mainInfo.js
@@ -5,6 +5,7 @@ import StatusMember from '../../components/statusMember';
 import { Ionicons } from '@expo/vector-icons';
 import { useFocusEffect } from '@react-navigation/core';
 import { ScrollView } from 'react-native-gesture-handler';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 
 const { height } = Dimensions.get("screen");
@@ -21,7 +22,7 @@ const MainInfor = ({ navigation, item }) => {
             <ScrollView>
                 <View>
                     <Image style={{
-                        height: logoHeight, width: '100%',
+                        height: height * 0.58, width: '100%',
                         borderTopLeftRadius: 10, borderTopRightRadius: 10,
                         borderBottomRightRadius: 50,
                         borderBottomLeftRadius: 50,
@@ -46,7 +47,7 @@ const MainInfor = ({ navigation, item }) => {
                             backgroundColor: 'whitesmoke',
                             alignSelf: 'center',
                             width: '90%',
-                            height: logoHeight * 0.2,
+                            height: logoHeight * 0.17,
                             borderRadius: 10,
                             flexDirection: 'row',
                             shadowOffset: { width: 1, height: 1 },
@@ -55,7 +56,6 @@ const MainInfor = ({ navigation, item }) => {
                             justifyContent: 'space-around',
                             paddingStart: 10,
                             paddingEnd: 10,
-
                         }}>
                             <View style={{ flexDirection: 'column', justifyContent: 'space-evenly', alignItems: 'center' }}>
                                 <Text style={{ fontFamily: 'nunitobold', fontSize: 18, color: 'black' }}>60</Text>
@@ -111,13 +111,43 @@ const MainInfor = ({ navigation, item }) => {
                             </View>
                         </View>
                     </View>
+                    <View style = {{
+                        marginTop: height*0.38,
+                        position: 'absolute',
+                         flexDirection: 'column',
+                         width: '90%',
+                         alignSelf: 'center',
+                         alignItems:'flex-start'
+                    }}>
+                        <View style={{
+                        borderRadius: 20,
+                        flexDirection: 'column',
+                        justifyContent: 'space-between',
+                        alignItems: 'flex-start',
+                        padding: 15,
+                        shadowOffset: { width: 1, height: 1 },
+                        shadowColor: 'black',
+                        shadowOpacity: 0.3,
+                        marginStart: 10
+                    }}>
+                        <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                            <Ionicons style ={{marginEnd: 10}} name="location" size={24} color="white" />
+                            <Text style={{ color: 'white', fontSize: 20, fontFamily: 'nunitobold' }}>Califonia</Text>
+                        </View>
+                        <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                            <FontAwesome5 style ={{marginEnd : 10}} name="birthday-cake" size={22} color="white" />
+                            <Text style={{ color: 'white', fontSize: 20, fontFamily: 'nunitobold' }}>27, August, 1998</Text>
+                        </View>
+
+
+                    </View>
+
 
                     <View style={{
-                        position: 'absolute',
+                        alignSelf: 'center',
                         backgroundColor: 'white',
-                        width: '90%',
+                        width: '100%',
                         height: logoHeight * 0.17,
-                        marginTop: height * 0.4,
                         borderRadius: 20,
                         alignSelf: 'center',
                         flexDirection: 'row',
@@ -148,7 +178,7 @@ const MainInfor = ({ navigation, item }) => {
                             <View style={{
                                 borderRadius: 20,
                                 padding: 7,
-                                backgroundColor: 'teal',
+                                backgroundColor: 'black',
                                 shadowOffset: { width: 1, height: 1 },
                                 shadowColor: 'black',
                                 shadowOpacity: 0.5,
@@ -157,6 +187,9 @@ const MainInfor = ({ navigation, item }) => {
                             </View>
                         </TouchableOpacity>
                     </View>
+
+                    </View>
+                   
                     <TouchableOpacity style={{ position: 'absolute', marginTop: 5, marginStart: 5 }} onPress={pressgobackHandler}>
                         <View style={{ flexDirection: 'row', marginBottom: 15, justifyContent: 'center', alignItems: 'center' }}>
                             <Ionicons name="chevron-back" size={30} color="white" />
@@ -176,10 +209,8 @@ const styles = StyleSheet.create({
         paddingStart: 10,
         paddingEnd: 10,
         paddingTop: 5,
-
         flex: 1,
         backgroundColor: 'whitesmoke'
-
 
     },
     button1: {
@@ -204,7 +235,7 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.2,
         borderRadius: 10,
         padding: 7,
-        backgroundColor: 'maroon'
+        backgroundColor: 'dimgrey'
     }
 
 });
