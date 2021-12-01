844f24a95383db1ebecf1c6c4246157e9da3c35e
pvdthien310 <71278418+pvdthien310@users.noreply.github.com>
2021-12-01 14:20:53 +0700

    ok

--- API/NotificationAPI.js
+++ API/NotificationAPI.js
@@ -18,11 +18,11 @@ const url = '/notification';
     },
     sendNoti : async newNoti => {
         const res = await DatabaseClient.post(url +'/send-data/',newNoti);
-        return res.data.json();
+        return res.data;
     },
     removeNoti : async removedNoti => {
         const res = await DatabaseClient.post(url +'/delete/', removedNoti);
-        return res.data.json();
+        return res.data;
     },
    
 }
