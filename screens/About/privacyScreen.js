e412e38ffc352807bf45ba077f51f6c5935c7164
Linh Nguyen <72326769+khanhlinh1406@users.noreply.github.com>
2021-12-15 14:25:05 +0700

    .

--- screens/About/privacyScreen.js
+++ screens/About/privacyScreen.js
@@ -3,6 +3,8 @@ import React, {
     useEffect
 } from 'react';
 
+import { Entypo } from '@expo/vector-icons';
+
 import {
     StyleSheet,
     Text,
@@ -25,7 +27,7 @@ const PrivacyScreen = ({ navigation }) => {
 
     return (
         <View style={styles.container}>
-           <SafeAreaView>
+            <SafeAreaView>
                 <View style={{
                     flexDirection: 'row',
                     alignItems: 'center'
@@ -65,7 +67,89 @@ const PrivacyScreen = ({ navigation }) => {
             </SafeAreaView>
 
             <ScrollView>
+                <View style={styles.view}>
+                    <Text style={styles.name}>{postPrivacy.name}</Text>
 
+                    <Text style={styles.itm}>{postPrivacy.itm1}</Text>
+
+                    <Text style={styles.content}>{postPrivacy.content1}</Text>
+
+                    <Text style={styles.itm}>{postPrivacy.itm1}</Text>
+
+                    <Text style={styles.content}>{postPrivacy.content1}</Text>
+
+                    <Text style={styles.content}>{postPrivacy.content2}</Text>
+                </View >
+
+                <View style={styles.view}>
+                    <Text style={styles.name}>{fraudPrivacy.name}</Text>
+
+                    <Text style={styles.content}>{fraudPrivacy.content}</Text>
+
+                    <View style={{
+                        flexDirection: 'row',
+                        marginLeft: 30
+                    }}>
+                        <Entypo name="dot-single" size={23} color="dimgray" />
+                        <Text style={...styles.content}>{fraudPrivacy.listDoNot[0]} </Text>
+                    </View>
+
+                    <View style={{
+                        flexDirection: 'row',
+                        marginLeft: 30
+                    }}>
+                        <Entypo name="dot-single" size={23} color="dimgray" />
+                        <Text style={...styles.content}>{fraudPrivacy.listDoNot[1]} </Text>
+                    </View>
+
+                    <View style={{
+                        flexDirection: 'row',
+                        marginLeft: 30
+                    }}>
+                        <Entypo name="dot-single" size={23} color="dimgray" />
+                        <Text style={...styles.content}>{fraudPrivacy.listDoNot[2]} </Text>
+                    </View>
+                </View>
+
+                <View style={styles.view}>
+                    <Text style={styles.name}>{wordPrivacy.name}</Text>
+
+                    <Text style={styles.content}>{wordPrivacy.content}</Text>
+
+                    <View style={{
+                        flexDirection: 'row',
+                        marginLeft: 30
+                    }}>
+                        <Entypo name="dot-single" size={23} color="dimgray" />
+                        <Text style={...styles.content}>{wordPrivacy.listDoNot[0]} </Text>
+                    </View>
+
+                    <View style={{
+                        flexDirection: 'row',
+                        marginLeft: 30
+                    }}>
+                        <Entypo name="dot-single" size={23} color="dimgray" />
+                        <Text style={...styles.content}>{fraudPrivacy.listDoNot[1]} </Text>
+                    </View>
+
+                    <View style={{
+                        flexDirection: 'row',
+                        marginLeft: 30
+                    }}>
+                        <Entypo name="dot-single" size={23} color="dimgray" />
+                        <Text style={...styles.content}>{fraudPrivacy.listDoNot[2]} </Text>
+                    </View>
+                </View>
+
+                <View style = {styles.view}>
+                    <Text style = {styles.name}>{rightPrivacy.name}</Text>
+
+                    <Text style = {styles.content}>{rightPrivacy.content}</Text>
+                </View>
+
+                <View style = {styles.view}>
+                    <Text style = {styles.thanks}>{thanks}</Text>
+                </View>
             </ScrollView>
         </View>
     )
@@ -83,16 +167,16 @@ const styles = StyleSheet.create({
     view: {
         margin: 15,
     },
-    title: {
+    name: {
         textAlign: 'left',
         fontFamily: 'nunitobold',
         fontSize: 20,
         color: 'black',
         marginTop: 5,
         marginRight: 10,
-    
+
     },
-    text: {
+    content: {
         fontFamily: 'nunitoregular',
         fontSize: 15,
         marginTop: 5,
@@ -118,37 +202,53 @@ export default PrivacyScreen
 const postPrivacy = {
     name: 'Post',
     itm1: 'What is Status Posts?',
-    content1: 'Status are posts with content about sharing feelings, personal experiences of the poster. Only when you are friends, you can see each others status.',
+    content1: 'Status are posts with content about sharing feelings, personal experiences of the poster.' +
+        ' Only when you are friends, you can see each others status.',
     itm2: 'What is Knowledge Posts?',
-    content2: 'Knowledge are articles sharing about common knowledge, in many fields such as education, culture, economy, science, entertainment ... that the writer wants to bring the value of knowledge to all Flâner users. . These articles must have clear grounds and evidence.',
-    content3: 'Users need to clearly distinguish the purpose of the two types of posts. We do not accept confusion between them. There are also some regulations like:'
+    content2: 'Knowledge are articles sharing about common knowledge, in many fields such as education, ' +
+        'culture, economy, science, entertainment ... that the writer wants to bring the value of knowledge to ' +
+        'all Flâner users. . These articles must have clear grounds and evidence.',
+    content3: 'Users need to clearly distinguish the purpose of the two types of posts. We do not accept' +
+        ' confusion between them. There are also some regulations below. Please read carefully so as not to make' +
+        ' mistakes, because if you make a mistake, we will take appropriate action!'
 }
 
 const fraudPrivacy = {
     name: 'Fraud',
-    content: 'We will remove content that has the purpose of deceiving, intentionally misrepresenting information or defrauding/taking advantage of others to obtain money/property, including content that seeks to mediate or promote acts. this through our service. Do not post:',
+    content: 'We will remove content that has the purpose of deceiving, intentionally misrepresenting ' +
+        'information or defrauding/taking advantage of others to obtain money/ property, including content that' +
+        ' seeks to mediate or promote acts. this through our service. Do not post:',
     listDoNot: [
-        'Defrauding others to gain financial or personal benefits in order to cause harm to individuals and organizations.',
+        'Defrauding others to gain financial or personal benefits in order to cause harm to individuals ' +
+        'and organizations.',
+
         'Collusion with others to acquire financial interests or personal interests',
+
         'Content participate, advocate, encourage, facilitate or admit the violation of law activities'
     ]
 }
 
-const wordPrivacy ={
+const wordPrivacy = {
     name: 'Words',
-    content: 'This is a problem that happens frequently. We define attack as violent or degrading language, harmful stereotypes, demeaning language, expressions of contempt, disgust or rejection, swearing, calls for boycotts, or calls for boycotts. isolation. With the desire to bring about a healthy community, we decided that users should not post:',
+    content: 'This is a problem that happens frequently. We define attack as violent or degrading language,' +
+        ' harmful stereotypes, demeaning language, expressions of contempt, disgust or rejection, swearing, calls' +
+        ' for boycotts, or calls for boycotts. isolation. With the desire to bring about a healthy community,' +
+        ' we decided that users should not post:',
     listDoNot: [
         'Do not use hate speech, swear words, insult any other personal or collective identity',
+
         'Public language insults , pro-violence',
+
         'Actions that boycott others'
     ]
 }
 
 const rightPrivacy = {
     name: 'Intellectual property rights',
-    content: 'Flâner does not allow people to post content that infringes on the intellectual property rights of others, including copyrights and trademarks goods.'
+    content: 'Flâner does not allow people to post content that infringes on the intellectual property rights' +
+        ' of others, including copyrights and trademarks goods.'
 }
 
-const thanks = 'Once again, we thank you for choosing Flâner. With the mission to bring you the best experience, '+
-'we will strive to perfect and improve Flâner even more. For a nice and healthy community, in addition to the development team, '+
-'we need you to work with us. Wish you the best!'
+const thanks = 'Once again, we thank you for choosing Flâner. With the mission to bring you the best experience, ' +
+    'we will strive to perfect and improve Flâner even more. For a nice and healthy community, in addition to the ' +
+    'development team, we need you to work with us. Wish you the best!'
