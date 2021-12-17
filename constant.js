421f6da1be0dcf270cfa2703253a202e3d573bc4
pvdthien310 <71278418+pvdthien310@users.noreply.github.com>
2021-12-17 14:27:26 +0700

    ok

--- constant.js
+++ constant.js
@@ -1,2 +1,2 @@
-export const URL_local = 'http://192.168.0.103:3000/api'
+export const URL_local = 'https://flanerapplication.herokuapp.com/api'
 export const URL_local_user = 'http://192.168.0.103:8000'
