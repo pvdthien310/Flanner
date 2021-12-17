980dcb0a0d6d8d64ac715d95588d92ceb6f8ec27
pvdthien310 <71278418+pvdthien310@users.noreply.github.com>
2021-12-17 21:00:48 +0700

    ok

--- server/AuthenServer.js
+++ server/AuthenServer.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 const bodyParser = require('body-parser')
 const UserRoute = require('./routes/userRoute')
 const JWTRoute = require('./routes/JWTRoute')
-// const JWTRefTokens = require('../server/models/JWTRefTokens')
+const JWTRefTokens = require('../server/models/JWTRefTokens')
 const jwt = require('jsonwebtoken')
 
 /// Process file json and env
@@ -53,45 +53,45 @@ app.use('/get-refreshToken', (req, res) => {
         res.json({ accessToken });
     })
 })
-// app.use('/get-accessToken', (req, res) => {
-//     const data = req.body;
-//     const accessToken = jwt.sign(data, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30s' });
-//     const refreshToken = jwt.sign(data, process.env.REFRESH_TOKEN_SECRET);
-//     refreshTokens.push(refreshToken);
-//     //// Update refreshTokens into database
-//     JWTRefTokens.findByIdAndUpdate('61a3b4c93b737600e720e39f',
-//         { "$push": { "refreshTokens": refreshToken } },
-//         { "new": true, "upsert": true }
-//     ).then((data) => {
-//         res.json({ accessToken, refreshToken });
+app.use('/get-accessToken', (req, res) => {
+    const data = req.body;
+    const accessToken = jwt.sign(data, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30s' });
+    const refreshToken = jwt.sign(data, process.env.REFRESH_TOKEN_SECRET);
+    refreshTokens.push(refreshToken);
+    //// Update refreshTokens into database
+    JWTRefTokens.findByIdAndUpdate('61a3b4c93b737600e720e39f',
+        { "$push": { "refreshTokens": refreshToken } },
+        { "new": true, "upsert": true }
+    ).then((data) => {
+        res.json({ accessToken, refreshToken });
        
-//     })
-//         .catch(err => console.log(err))
-//     //
-// })
+    })
+        .catch(err => console.log(err))
+    //
+})
 
-// app.use('/logout', (req, res) => {
-//     const refreshToken = req.body.token;
-//     refreshTokens = refreshTokens.filter((refToken) => refToken !== refreshToken);
-//     ///update Update refreshTokens into database 
-//     JWTRefTokens.findByIdAndUpdate('61a3b4c93b737600e720e39f',
-//         { "$pull": { "refreshTokens": refreshToken } },
-//         { "new": true, "upsert": true }
-//     ).then((data) => {
-//         res.sendStatus(200)
-//     })
-//         .catch(err => console.log(err))
-//     //
-// })
+app.use('/logout', (req, res) => {
+    const refreshToken = req.body.token;
+    refreshTokens = refreshTokens.filter((refToken) => refToken !== refreshToken);
+    ///update Update refreshTokens into database 
+    JWTRefTokens.findByIdAndUpdate('61a3b4c93b737600e720e39f',
+        { "$pull": { "refreshTokens": refreshToken } },
+        { "new": true, "upsert": true }
+    ).then((data) => {
+        res.sendStatus(200)
+    })
+        .catch(err => console.log(err))
+    //
+})
 
 //// Open port
 const port = process.env.PORT || 8800
 app.listen(port, () => {
-    // JWTRefTokens.find({})
-    //     .then(data => {
-    //         refreshTokens = data[0].refreshTokens
-    //     })
-    //     .catch(err => console.log(err))
+    JWTRefTokens.find({})
+        .then(data => {
+            refreshTokens = data[0].refreshTokens
+        })
+        .catch(err => console.log(err))
     console.log('AuThen backends server is running!')
 })
 
